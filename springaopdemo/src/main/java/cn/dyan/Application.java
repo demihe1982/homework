41c8ca6c35acc8346a0bf7030b4f50137f317e01
dyanHe <mynol.xu@ericsson.com>
2017-09-15 21:45:55 +0800

    add dependence

---
+++ springaopdemo/src/main/java/cn/dyan/Application.java
@@ -0,0 +1,16 @@
+package cn.dyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by demi on 2017/9/15.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
